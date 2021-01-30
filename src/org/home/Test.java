914bbef9200ff99765faa731e0f5664e51a6ee5e
PRADYUMNA KUMARDIXIT <PRADYUMNA KUMARDIXIT@LAPTOP-76S5CG4M>
2021-01-30 09:53:55 +0530

    First commit....

---
+++ src/org/home/Test.java
@@ -0,0 +1,16 @@
+package org.home;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if (hungry == "yes")
+		{
+			System.out.println("Eat Something");
+		}
+		else
+		{
+			System.out.println("Do Work....");
+		}
+	}
+}
